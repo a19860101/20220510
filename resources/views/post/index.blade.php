b92de7c34283e0bef44c1550e765e9b1c1ee0ea4
Zac <a19860101@gmail.com>
2022-05-10 21:00:37 +0800

    20220510

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -8,10 +8,14 @@
 </head>
 <body>
     <h1>POST INDEX</h1>
-    <a href="post/create">create</a>
+    <a href="/post/create">create</a>
+    <a href="{{route('post.create')}}">create</a>
 
-
-    <a href="post/1">post 1</a>
-    <a href="post/55">post 55</a>
+    <?php
+        $id = 88;
+    ?>
+    <a href="/post/1">post 1</a>
+    <a href="/post/{{$id}}">post 55</a>
+    <a href="{{route('post.show',['post'=>66])}}">post 66</a>
 </body>
 </html>
