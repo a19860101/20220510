6a05aca6c20a1b19fd6f02bffcffb8ab04f12cee
Zac <a19860101@gmail.com>
2022-05-10 20:37:42 +0800

    20220510

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>POST INDEX</h1>
+    <a href="post/create">create</a>
+
+
+    <a href="post/1">post 1</a>
+    <a href="post/55">post 55</a>
+</body>
+</html>
