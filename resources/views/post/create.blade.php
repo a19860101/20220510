00872aabf0b62320d1abfda752f32cbf0856be0a
Zac <a19860101@gmail.com>
2022-05-12 19:27:17 +0800

    20220512

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,1 +1,25 @@
 @extends('template.master')
+@section('main')
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <form action="" method="post">
+                @csrf
+                <div class="mb-3">
+                    <label for="" class="form-label">標題</label>
+                    <input type="text" name="title" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">封面</label>
+                    <input type="file" name="cover" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">內文</label>
+                    <textarea name="content" id="" cols="30" rows="10" class="form-control"></textarea>
+                </div>
+                <input type="submit" class="btn btn-primary">
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
