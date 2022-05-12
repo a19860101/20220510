0803eedb6cc6619bc87488d477e2c94222bf5371
Zac <a19860101@gmail.com>
2022-05-12 19:10:32 +0800

    20220512

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -1,16 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>POST EDIT</h1>
-    <h2><?php echo $tid; ?></h2>
-    <h2><?php echo $t_test; ?></h2>
-    <h2>{{$tid}}</h2>
-    <h2>{{$t_test}}</h2>
-</body>
-</html>
+@extends('template.master')
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>POST INDEX</h1>
-    <a href="/post/create">create</a>
-    <a href="{{route('post.create')}}">create</a>
-    <?php
-        $id = 88;
-    ?>
-    <a href="/post/1">post 1</a>
-    <a href="/post/{{$id}}">post 55</a>
-    <a href="{{route('post.show',['post'=>66])}}">post 66</a>
-</body>
-</html>
+@extends('template.master')
+@section('main')
+<h1>index</h1>
+@endsection
