0435fd936f91ac676f92c106cbaa8526a8f87b1b
Zac <a19860101@gmail.com>
2022-05-17 19:29:29 +0800

    20220517

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,4 +1,37 @@
 @extends('template.master')
 @section('main')
-<h1>product create</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <form action="{{route('product.store')}}" method="post">
+                @csrf
+                <div>
+                    <label for="" class="form-label">商品名稱</label>
+                    <input type="text" class="form-control" name="title">
+                </div>
+                <div>
+                    <label for="" class="form-label">商品敘述</label>
+                    <textarea name="desc" id="" cols="30" rows="10" class="form-control"></textarea>
+                </div>
+                <div>
+                    <label for="" class="form-label">價格</label>
+                    <input type="text" class="form-control" name="price">
+                </div>
+                <div>
+                    <label for="" class="form-label">特價</label>
+                    <input type="text" class="form-control" name="sale">
+                </div>
+                <div>
+                    <label for="" class="form-label">上架</label>
+                    <input type="date" class="form-control" name="started_at">
+                </div>
+                <div>
+                    <label for="" class="form-label">下架</label>
+                    <input type="date" class="form-control" name="ended_at">
+                </div>
+                <input type="submit" value="新增商品" class="btn btn-primary">
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
